2c40beffcdd62929db43ea728717ae33a8b370be
Ksenofontov Gregory <geks333@yandex.ru>
2023-03-15 18:24:14 +0300

    Initialize project template

---
+++ docs/source/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/market-regression'))
+
+
+project = 'Sberbank Russian Housing Market'
+copyright = '2023, Ksenofontov Gregory'
+author = 'Ksenofontov Gregory'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Turn off prepending module names
+add_module_names = False
+# Sort members by type
+autodoc_member_order = 'groupwise'
+# Document __init__, __repr__, and __str__ methods
+def skip(app, what, name, obj, would_skip, options):
+    if name in ("__init__", "__repr__", "__str__"):
+        return False
+    return would_skip
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
