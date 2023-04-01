9008cbda81f2139ba26b2558d1a23ce96b4fed7c
Ksenofontov Gregory <geks333@yandex.ru>
2023-04-01 03:17:45 +0300

    Removed exessive asserts

--- test/test_marketreg.py
+++ test/test_marketreg.py
@@ -19,13 +19,9 @@ def test_preprocessing():
     #=================================
     # Test type of raw data
     msg = marketreg.preprocess([0, 1, 2, 3])
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Raw data type must be pd.DataFrame"
 
     # Test the columns
     msg = marketreg.preprocess(pd.DataFrame())
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Wrong columns"
 
 def test_predict():
     """
@@ -36,24 +32,18 @@ def test_predict():
     #=================================
     # Test model type
     msg = marketreg.predict(xgb.XGBClassifier(), np.ones(shape=(1, 298)))
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Model type must be xgb.sklearn.XGBRegressor"
 
     # Test that model is not fitted
     msg = marketreg.predict(xgb.XGBRegressor(), np.ones(shape=(1, 298)))
-    assert isinstance(msg, NotFittedError)
+
 
     test_X = np.ones(shape=(1, 292))
     test_y = np.ones(shape=(1,)) 
     # Test data type
     msg = marketreg.predict(xgb.XGBRegressor().fit(test_X, test_y), [0, 1, 2, 3])
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Input data type must be np.ndarray"
 
     # Test data length
     msg = marketreg.predict(xgb.XGBRegressor().fit(test_X, test_y), np.ones(shape=(1, 292)))
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Wrong features length"
 
     
 
@@ -62,7 +52,7 @@ def test_train():
     Tests for various assertion cheks written in the train function
     """
     #=================================
-    # TEST SUITE
+    # ASSERTATION TEST SUITE
     #=================================
     # Test model type
     msg = marketreg.train(xgb.XGBClassifier(), 
@@ -70,8 +60,6 @@ def test_train():
                             np.ones(shape=(1, )),
                             np.ones(shape=(1, 298)),
                             np.ones(shape=(1, )),)
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Model type must be xgb.sklearn.XGBRegressor"
 
     # Test train data type
     msg = marketreg.train(xgb.XGBRegressor(), 
@@ -80,8 +68,6 @@ def test_train():
                             np.ones(shape=(1, 298)),
                             np.ones(shape=(1, ))
                             )
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Train data type must be np.ndarray"
 
     # Test train label type
     msg = marketreg.train(xgb.XGBRegressor(), 
@@ -90,8 +76,6 @@ def test_train():
                             np.ones(shape=(1, 298)),
                             np.ones(shape=(1, ))
                             )
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Train label type must be np.ndarray"
 
     # Test validation data type
     msg = marketreg.train(xgb.XGBRegressor(), 
@@ -100,8 +84,6 @@ def test_train():
                             [1],
                             np.ones(shape=(1, ))
                             )
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Validation data type must be np.ndarray"
 
     # Test validation label type
     msg = marketreg.train(xgb.XGBRegressor(), 
@@ -110,8 +92,6 @@ def test_train():
                             np.ones(shape=(1, 298)),
                             [1]
                             )
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Validation label type must be np.ndarray"
 
     # Test validation label type
     msg = marketreg.train(xgb.XGBRegressor(), 
@@ -120,8 +100,6 @@ def test_train():
                             np.ones(shape=(1, 298)),
                             np.ones(shape=(1, )),
                             )
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Train data wrong shape"
 
     # Test validation label type
     msg = marketreg.train(xgb.XGBRegressor(), 
@@ -130,8 +108,12 @@ def test_train():
                             np.ones(shape=(1, 2)),
                             np.ones(shape=(1, )),
                             )
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Validation data wrong shape"
+
+    #=================================
+    # MODEL TEST SUITE
+    #=================================
+    # Test R^2
+    
 
 
 def test_get_data():
@@ -143,5 +125,3 @@ def test_get_data():
     #=================================
     # Test model type
     msg = marketreg.get_data(1)
-    assert isinstance(msg, AssertionError)
-    assert msg.args[0] == "Data path must be string"
