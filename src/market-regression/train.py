8a9bd5f5916a7e7039eb4e788052495a34ce508d
Ksenofontov Gregory <geks333@yandex.ru>
2023-04-01 04:23:40 +0300

    Added new variable

--- src/market-regression/train.py
+++ src/market-regression/train.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         data_path, save_path = sys.argv[1:]
         X, X_val, y, y_val = marketreg.get_data(data_path)
         model = marketreg.build_model()
-        model = marketreg.train(model, X, y, X_val, y_val)
+        model, _ = marketreg.train(model, X, y, X_val, y_val)
         model.save_model(os.path.join(save_path, 'model.json'))
