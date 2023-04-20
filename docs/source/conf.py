dacac1a93d4d81d14dde6f01a916f7e06751b529
Ksenofontov Gregory <geks333@yandex.ru>
2023-04-20 04:11:43 +0300

    Removed unecessary variable

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,4 +45,3 @@ napoleon_attr_annotations = True
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
