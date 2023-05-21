553828a97e3e06218ce4a0ab386cfaadb606cbdd
Ksenofontov Gregory <geks333@yandex.ru>
2023-05-21 12:28:27 +0300

    Used argparce to parce arguments, changed loading of a model using new method, now script saves preds

--- src/market-regression/predict.py
+++ src/market-regression/predict.py
@@ -6,51 +6,33 @@ Example
 -------
 ::
 
-    [~]$ python predict.py <data_location> <model_location>
-    +----------------+-------------------+
-    | Transaction ID | Approximate Price |
-    +----------------+-------------------+
-    |     30474      |      5709956      |
-    |     30475      |      8662689      |
-    |     30476      |      5963731      |
-    |     30477      |      6130314      |
-    |     30478      |      5387550      |
-    |     30479      |      9045907      |
-    |     38135      |      9206236      |
-    +----------------+-------------------+
-
-
-Notes
------
-    The predicted price could be saved in file using ``<command> > result.txt``.
+    [~]$ python predict.py <data_path> <model_path> <save_path>
+    [~]$ ls <save_path>
+    predictions.npy
 
 """
 
 
 import os
-import sys
+import numpy as np
 import pandas as pd
-from xgboost import XGBRegressor
-from prettytable import PrettyTable
+import argparse
 
 import marketreg
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 3:
-        data_path, model_path = sys.argv[1:]
+    parser = argparse.ArgumentParser(description='Predict houshold prices')
+    parser.add_argument('data_path', type=str, help='Path of data to predict')
+    parser.add_argument('model_path', type=str, help='Path of trained model')
+    parser.add_argument('save_path', type=str, help='Path of trained model')
+    args = parser.parse_args()
 
-        model = XGBRegressor()
-        model.load_model(os.path.join(model_path))
+    model = marketreg.load_model(os.path.join(args.model_path))
 
-        data = pd.read_csv(data_path, parse_dates=['timestamp'])
-        ids = data["id"]
+    data = pd.read_csv(args.data_path, parse_dates=['timestamp'])
 
-        data = marketreg.preprocess(data)
+    data = marketreg.preprocess(data)
 
-        preds = marketreg.predict(model, data.to_numpy()).astype(int)
-        
-        pt = PrettyTable()
-        pt.field_names = ["Transaction ID", "Approximate Price"]
-        pt.add_rows(zip(ids, preds))
-        print(pt)
-        
+    preds = marketreg.predict(model, data.to_numpy()).astype(int)
+    np.save(os.path.join(args.save_path, 'predictions.npy'), preds)
+    
