247f04dd4ffe3c8288d19518119f04656210683d
Ksenofontov Gregory <geks333@yandex.ru>
2023-05-21 12:29:59 +0300

    Used argparce to parce arguments

--- src/market-regression/train.py
+++ src/market-regression/train.py
@@ -14,14 +14,16 @@ Example
 """
 
 import os
-import sys
+import argparse
 import marketreg
 
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 3:
-        data_path, save_path = sys.argv[1:]
-        X, X_val, y, y_val = marketreg.get_data(data_path)
-        model = marketreg.build_model()
-        model, _ = marketreg.train(model, X, y, X_val, y_val)
-        marketreg.save_model(model, os.path.join(save_path, 'model.json'))
+    parser = argparse.ArgumentParser(description='Train model to predict houshold prices')
+    parser.add_argument('data_path', type=str, help='Path of train data')
+    parser.add_argument('save_path', type=str, help='Path to save trained model')
+    args = parser.parse_args()
+    X, X_val, y, y_val = marketreg.get_data(args.data_path)
+    model = marketreg.build_model()
+    model, _ = marketreg.train(model, X, y, X_val, y_val)
+    marketreg.save_model(model, os.path.join(args.save_path, 'model.json'))
